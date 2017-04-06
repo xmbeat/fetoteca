b194fead69f17fb1fc52418be1be75742599d85c
Xaco <hailxaco@gmail.com>
2017-04-06 13:15:36 -0500

    paginacion

--- fetoteca/apps/embriologia/urls.py
+++ fetoteca/apps/embriologia/urls.py
@@ -8,4 +8,6 @@ app_name = "embriologia"
 urlpatterns = [	
     url(r'^lista$', views.lista, name='lista'),
     url(r'^$', views.index, name='index'), 
+    url(r'^lista_fetos$', views.lista_fetos, name='lista_fetos'),
+    url(r'^ver/([0-9]+)/$', views.ver, name='ver'),
 ]
--- fetoteca/apps/embriologia/views.py
+++ fetoteca/apps/embriologia/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
 from .models import Feto, Malformacion, ImagenFeto
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger #puesto por xaco
+from pprint import pprint
+
 # Create your views here.
 def lista(request):
 	context = {}
@@ -13,3 +16,24 @@ def lista(request):
 def index(request):
 	context = {}
 	return render (request,'index.html',context)
+
+#puesto por xaco
+def lista_fetos(request):
+    fetos_lista = Feto.objects.all()
+    paginator = Paginator(fetos_lista, 1) 
+    pagina = request.GET.get('page')#incluir parametro en template
+
+    try:
+        fetos = paginator.page(pagina)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        fetos = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        fetos = paginator.page(paginator.num_pages)
+    
+    rango=range(1,paginator.num_pages+1)
+    return render(request, 'lista.html', {'fetos': fetos,'rango':rango})
+
+def ver(request,id):
+    pass
